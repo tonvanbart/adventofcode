9efd3236a1cde580126f8c3c52ec0011916cfff4
Ton van Bart <tonvanbart@gmail.com>
2022-12-06 12:05:16 +0100

    day 6

--- src/main/java/advent2022/Day6.java
+++ src/main/java/advent2022/Day6.java
@@ -2,6 +2,8 @@ package advent2022;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day6 {
     public static void main(String[] args) throws Exception {
@@ -13,6 +15,38 @@ public class Day6 {
         var file = "input";
         final var lines = Files.readAllLines(Path.of(pfx, file));
         System.out.println("lines.size() = " + lines.size());
+        String input = lines.get(0);
+        for (int i = 4; i<input.length()+1; i++) {
+            var test = input.substring(i - 4, i);
+            if (isUnique(test)) {
+                System.out.println("unique at "+i);
+                break;
+            }
+        }
+        for (int i = 14; i<input.length()+1; i++) {
+            var test = input.substring(i - 14, i);
+            if (unique14(test)) {
+                System.out.println("unique14 at "+i);
+                break;
+            }
+        }
+    }
+    
+    boolean isUnique(String str) {
+//        System.out.println("isUnique("+str+")");
+        Set<Character> chars = new HashSet<>();
+        for (Character c: str.toCharArray()) {
+            chars.add(c);
+        }
+        return chars.size() == 4;
+    }
 
+    boolean unique14(String str) {
+        System.out.println("isUnique("+str+")");
+        Set<Character> chars = new HashSet<>();
+        for (Character c: str.toCharArray()) {
+            chars.add(c);
+        }
+        return chars.size() == 14;
     }
 }
